469ad3dffbb38ef8ce5ecc9266bd1e07ab4dc0a1
Justin <justin.warambourg@gmail.com>
2024-06-04 14:42:04 +0800

    update collection id

--- location/db_client.py
+++ location/db_client.py
@@ -22,23 +22,26 @@ class MongoDB:
 
 class SpotDB(MongoDB):
     def __init__(self, user: str, pw: str) -> None:
-        super().__init__(user, pw, "location")
+        super().__init__(user, pw, "location-stage")
+        self.collection_id = "spot"
 
     def create_spot(self, payload: dict):
         """Create a new entry in the database. Payload must adhere to the Spot JSON Schema"""
-        return self.database["test"].insert_one(payload)
+        return self.database[self.collection_id].insert_one(payload)
 
     def get_spots(self, str_id: list[str]):
         try:
             object_ids = [ObjectId(id) for id in str_id]
         except errors.InvalidId as e:
             raise ValueError(f"Invalid id provided")
-        cursor_result = self.database["test"].find({"_id": {"$in": object_ids}})
+        cursor_result = self.database[self.collection_id].find(
+            {"_id": {"$in": object_ids}}
+        )
         return [json.loads(json_util.dumps(spot)) for spot in cursor_result]
 
     def search_spots(self, filter):
         """Thin wrapper around MongoDB.find()"""
-        cursor_result = self.database["test"].find(filter)
+        cursor_result = self.database[self.collection_id].find(filter)
 
         return [json.loads(json_util.dumps(obj)) for obj in cursor_result]
 
