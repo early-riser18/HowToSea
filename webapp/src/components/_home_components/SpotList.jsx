213a91cf85212b855944c72dde88655e62454e10
Justin <justin.warambourg@gmail.com>
2024-06-11 13:27:37 +0800

    add dynamic spotlist

--- webapp/src/components/_home_components/SpotList.jsx
+++ webapp/src/components/_home_components/SpotList.jsx
@@ -1,52 +1,42 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function SpotList() {
-  const spot = {
-    _id: {
-      $oid: "665eaae9d466af1e346117c7",
-    },
-    author_id: "1",
-    title: "Cap gros",
-    dateCreated: "13/10/2020",
-    description:
-      "Zone escarpée, la mise à l'eau peut-être difficile.\ndans la baie à la droite du cap il y a possibilité de s'abriter du courant et de trouver des profondeurs inférieures à 10m de fond avec de beaux poissons tout de même.\nAu bout du cap se situe un plateau à environ 8m de fond suivit du fameux tombant pouvant aller jusqu'à 30m de profondeur, parfait pour un freefall ou simplement pour rester sur le plateau et observer le passage de dorades, sars, mérous et pour les chanceux Dentis et Barracuda.\nDe magnifiques Gorgones ornent le tombant.",
-    image: [
-      "https://firebasestorage.googleapis.com/v0/b/diving-app-eaabe.appspot.com/o/43.5519%2Fcapgros1.jpg?alt=media&token=81c7b462-b283-45c3-8e7d-59b30ef103de",
-      "/spot_img/cap_gros_1.jpg",
-      "/spot_img/cap_gros_2.jpg",
-    ],
-    isPublished: true,
-    characteristics: {
-      adaptedFor: "all",
-      fishy: "true",
-      reef: "true",
-      shipwreck: "false",
-      wall: "false",
-      depth: "20 to 39m",
-      recommendedAccess: "foot",
-    },
-    updateTs: "13/10/2020",
-    latitude: 43.551923,
-    longitude: 7.144933,
-    level: "hard",
-    parking:
-      "Se garer sur l'Avenue André Sella ou au parking de la plage Keller.",
-    access_instructions:
-      "Descendre la rue en direction de la plage puis se diriger vers la droite jusqu'à l'entrée du sentier du littoral. \nSuivre le sentier du littoral pendant une 20 aine de minute jusqu'au cap gros (1er cap après le passage d'une porte le long d'un mur).",
-    address: {
-      street: "Avenue André Sella",
-      postalCode: "06600",
-      city: "Antibes",
-      country: "France",
-    },
-  };
+  const [spotsData, setSpotsData] = useState([]);
+  useEffect(() => {
+    console.log("i am called");
+    const options = {
+      method: "GET", // HTTP method
+      headers: {
+        "Content-Type": "application/json", // Specify the content type
+      },
+    };
+    const url = `${process.env.NEXT_PUBLIC_WEB_SERVER_URL}/location/rec`;
+    fetch(url, options)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok " + response.statusText);
+        }
+        return response.json();
+      })
+      .then((body) => {
+        console.log(body);
+        setSpotsData(body.data);
+      })
+      .catch((error) => {
+        console.error("An error occured while talking to the server.", error);
+      });
+  }, []);
   return (
     <div>
       <h2 className="my-10 font-header text-2xl text-primary md:text-3xl">
         NOS SPOTS À LA UNE
       </h2>
       <div className="mb-20 flex flex-wrap justify-around">
-        <SpotMiniature spot={spot} />
-        <SpotMiniature spot={spot} />
-        <SpotMiniature spot={spot} />
+        {spotsData.map((spot) => (
+          <SpotMiniature spot={spot} />
+        ))}
       </div>
     </div>
   );
