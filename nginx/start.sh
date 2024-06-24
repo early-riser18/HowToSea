d706d4e4e57078e867bc034e7d7bd4373eea73c0
Justin <justin.warambourg@gmail.com>
2024-06-24 15:20:43 +0800

    add location to proxy lambda_api

--- nginx/start.sh
+++ nginx/start.sh
@@ -2,6 +2,6 @@
 
 # You can add any number of variables below
 
-envsubst '$DNS_ADDRESS,$LOCATION_HOSTNAME,' < /etc/nginx/conf.d/default.template > /etc/nginx/nginx.conf
+envsubst '$DNS_ADDRESS,$LOCATION_HOSTNAME,$LAMBDA_API_URL' < /etc/nginx/conf.d/default.template > /etc/nginx/nginx.conf
 
 exec nginx -g 'daemon off;'
