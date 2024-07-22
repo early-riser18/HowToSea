16e6f7f1d772228f29f270aae2a69b9c9cb333e0
Justin <justin.warambourg@gmail.com>
2024-07-22 15:06:43 +0200

    styling: searchbar search page

--- webapp/src/app/search/SearchBar.jsx
+++ webapp/src/app/search/SearchBar.jsx
@@ -1,38 +1,89 @@
-import useSearchSpotForm from "../utils/useSearchSpotForm";
-import PlaceAutocomplete from "../utils/PlaceAutocomplete";
-import { levelOptions, radiusOptions } from "@/app/utils/spotSearchOptions";
+"use client";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import useSearchSpotForm from "@/app/utils/useSearchSpotForm";
+import PlaceAutocomplete from "@/app/utils/PlaceAutocomplete";
+import { radiusOptions, levelOptions } from "@/app/utils/spotSearchOptions";
 
-export default function SearchBar({ handleSubmit }) {
+export default function HomeSearchBar({ handleSubmit }) {
   const { placeField, onSubmit, onPlaceSelect, onChangePlaceField } =
     useSearchSpotForm(handleSubmit);
 
   return (
     <>
-      <form className="flex" onSubmit={onSubmit}>
-        <PlaceAutocomplete
-          onPlaceSelect={onPlaceSelect}
-          onChange={onChangePlaceField}
-          placeholder="Où veux-tu plonger?"
-        >
-          {placeField}
-        </PlaceAutocomplete>
+      <form
+        className="rounder-md my-3 inline-flex items-center border-slate-200 bg-slate-100 px-2"
+        onSubmit={onSubmit}
+      >
+        <FormInput
+          myLabel={<Label>Lieu</Label>}
+          myInput={
+            <PlaceAutocomplete
+              onPlaceSelect={onPlaceSelect}
+              onChange={onChangePlaceField}
+              placeholder="Où veux-tu plonger?"
+            >
+              {placeField}
+            </PlaceAutocomplete>
+          }
+        />
+        <FormInput
+          myLabel={<Label>Distance</Label>}
+          myInput={
+            <select
+              className="w-32 rounded-md border-[1px] border-gray-200 bg-slate-50 p-1"
+              id="rad"
+            >
+              {radiusOptions.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+          }
+        />
+        <FormInput
+          myLabel={<Label>Niveau</Label>}
+          myInput={
+            <select
+              className="w-32 rounded-md border-[1px] border-gray-200 bg-slate-50 p-1"
+              id="level"
+            >
+              {levelOptions.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+          }
+        />
 
-        <select id="rad">
-          {radiusOptions.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </select>
-        <select id="level">
-          {levelOptions.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </select>
-        <button type="submit">Rechercher</button>
+        <div className="bg-gradient-to-r my-2 flex items-center justify-center rounded-xl bg-blue-400 px-5 py-3 text-white md:mr-2 md:rounded-lg">
+          <button type="submit" className="flex items-center">
+            <FontAwesomeIcon icon={faMagnifyingGlass} className="mr-2" />
+            Rechercher
+          </button>
+        </div>
       </form>
     </>
+  );
+}
+
+function Label({ children, myHtmlFor }) {
+  return (
+    <label
+      className="mb-1.5 text-xs font-semibold uppercase"
+      htmlFor={myHtmlFor}
+    >
+      {children}
+    </label>
+  );
+}
+function FormInput({ myLabel, myInput }) {
+  return (
+    <div className="flex w-full flex-1 flex-col flex-wrap rounded-xl bg-inherit bg-white px-4 py-3 md:bg-transparent">
+      {myLabel}
+      {myInput}
+    </div>
   );
 }
