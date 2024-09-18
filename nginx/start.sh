733ec001375cc84cc4083e639d6ac52406a78670
Justin <justin.warambourg@gmail.com>
2024-09-18 10:15:06 +0800

    enable env file to be sourced at container launch

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# You can add any number of variables below
+# Following script is necessary since .env file is not "sourced" at container launch in current ECS config.
+if [ -f /.env ]; then
+  export $(grep -v '^#' /.env | xargs)
+fi
 
 envsubst '$DNS_ADDRESS,$LOCATION_HOSTNAME,$LAMBDA_API_URL,$AUTH_HOSTNAME' < /etc/nginx/conf.d/default.template > /etc/nginx/nginx.conf
 
